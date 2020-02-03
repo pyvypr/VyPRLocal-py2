4d223c9e0b49416ef6c985a2a36c73c7392c0861
Joshua Dawes <joshua.dawes@cern.ch>
2020-02-03 15:32:35 +0100

    Fix import - wrong Python version.

--- init_verification.py
+++ init_verification.py
@@ -9,10 +9,7 @@ import threading
 import traceback
 
 # this differs between versions...
-try:
-    from queue import Queue
-except:
-    import Queue
+from Queue import Queue
 import requests
 from VyPR.SCFG.construction import CFGEdge, CFGVertex
 from VyPR.QueryBuilding import *
