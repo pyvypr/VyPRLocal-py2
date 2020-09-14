d162c70b11c9521af6d70a4ac5ccefee4655c440
Joshua Dawes <joshua.dawes@cern.ch>
2020-09-14 17:35:19 +0200

    Add missing config file.

---
+++ config.py
@@ -0,0 +1,14 @@
+"""
+Global configuration variables.
+"""
+
+VERDICT_SERVER_URL = None
+VERBOSE = False
+EXPLANATION = False
+DRAW_GRAPHS = False
+BYTECODE_EXTENSION = ".pyc"
+vypr_module = "."
+VERIFICATION_INSTRUCTION = "verification.send_event"
+LOGS_TO_STDOUT = False
+VYPR_OUTPUT_VERBOSE = True
+PROJECT_ROOT = None
